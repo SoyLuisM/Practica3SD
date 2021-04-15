0273057b9e22e9509a86fd200cfeb9b41721eb12
Luis Martinez <s_k_ap3@hotmail.com>
2021-04-15 08:21:50 -0500

    arreglando pequeño bug por el que no mostraba los valores generados

--- codigosPractica/sin distribuir/random_funcion.c
+++ codigosPractica/sin distribuir/random_funcion.c
@@ -1,13 +1,12 @@
 /* Archivo random_funcion.c que incluye la definición de las funciones a utilizar */
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>#include "random_local.h"
+#include <unistd.h>
+#include "random_local.h"
 
-void inicializa_random(long semilla)
-{
-srandom(semilla);
+void inicializa_random(long semilla){
+      srandom(semilla);
 }
-double obtiene_siguiente_random(void)
-{
-return random();
+double obtiene_siguiente_random(void){
+      return random();
 }
--- codigosPractica/sin distribuir/random_local.c
+++ codigosPractica/sin distribuir/random_local.c
@@ -4,17 +4,20 @@
 #include <unistd.h>
 #include "random_local.h"
 
-int main(int argc, char *argv[])
-{
-int misemilla, itera, i;
+int main(int argc, char *argv[]){
+	int misemilla, itera, i;
+
 	if(argc != 3) {
-	printf("Uso: %s semilla iteraciones\n", argv[0]);
-	exit(1);
-}
+		printf("Uso: %s semilla iteraciones\n", argv[0]);
+		exit(1);
+	}
 	misemilla = (long)atoi(argv[1]);
 	itera = atoi(argv[2]);
 	inicializa_random(misemilla);
-	for(i = 0; i < itera; i++)
-		printf("%d : %d\n", i, obtiene_siguiente_random());
+
+	for(i = 0; i < itera; i++){
+		printf("%d : %d\n", i, (int)obtiene_siguiente_random());
+	}
+
 	exit(0);
 }
